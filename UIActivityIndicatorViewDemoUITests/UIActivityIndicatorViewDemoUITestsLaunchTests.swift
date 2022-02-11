fe2639cfcf7ed06e5cfe3ad85fc52d60e0c1cbc2
bennett.chan <bennett.chan@tbcasoft.com>
2022-02-11 09:57:52 +0800

    Initial Commit

---
+++ UIActivityIndicatorViewDemoUITests/UIActivityIndicatorViewDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UIActivityIndicatorViewDemoUITestsLaunchTests.swift
+//  UIActivityIndicatorViewDemoUITests
+//
+//  Created by 竣亦 on 2022/2/11.
+//
+
+import XCTest
+
+class UIActivityIndicatorViewDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
