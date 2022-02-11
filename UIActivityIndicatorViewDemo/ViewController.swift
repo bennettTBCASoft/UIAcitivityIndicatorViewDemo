3631465ca399db6ce8abad247d50ebca6c33703d
bennett.chan <bennett.chan@tbcasoft.com>
2022-02-11 10:15:18 +0800

    Completed the demo

--- UIActivityIndicatorViewDemo/ViewController.swift
+++ UIActivityIndicatorViewDemo/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var upIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var downIndicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // upIndicator
+        upIndicator.style = .medium
+        upIndicator.hidesWhenStopped = true
+        upIndicator.startAnimating()
+        self.upIndicator.color = .blue
+        
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { Timer in
+            self.upIndicator.stopAnimating()
+        }
+        
+        
+        // downIndicator
+        downIndicator.style = .large
+        downIndicator.hidesWhenStopped = false
+        downIndicator.color = .red
+        self.downIndicator.startAnimating()
+        
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { Timer in
+            self.downIndicator.stopAnimating()
+        }
     }
 
 
+    
 }
 
